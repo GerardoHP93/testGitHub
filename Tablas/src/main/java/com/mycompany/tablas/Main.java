4f39a536282976a38545a47990800d2efaed3695
Gerardo Herrera <Gerardo Herrera@10.182.3.80>
2023-04-18 15:33:02 -0500

    Se refactoriza los métodos propios de la clase municipio y se eliminan de la clase main

--- Tablas/src/main/java/com/mycompany/tablas/Main.java
+++ Tablas/src/main/java/com/mycompany/tablas/Main.java
@@ -6,6 +6,7 @@ package com.mycompany.tablas;
 
 import java.util.ArrayList;
 import models.Municipio;
+import static models.Municipio.*;
 
 /**
  *
@@ -13,41 +14,7 @@ import models.Municipio;
  */
 public class Main {
     
-    static ArrayList<Municipio> municipios = new ArrayList<>();
-    
-    public static void llenarMunicipios(){
-        municipios.add(new Municipio(1,"Calkini"));
-        municipios.add(new Municipio(2,"Campeche"));
-        municipios.add(new Municipio(3,"Carmen"));
 
-        System.out.println(municipios);
-        
-        
-    }
-    
-    
-    public static void listaMunicipiosForE(){
-        System.out.println("For each");
-        for (Municipio municipio : municipios) {
-            System.out.println(municipio);
-        }
-    }
-    
-    public static void listaMunicipiosForI(){
-        System.out.println("forI");
-        for (int i = 0; i < municipios.size(); i++) {
-            System.out.println(municipios.get(i).toString());
-        }
-    }
-    
-    public static void eliminarMunicipios(int id){
-        municipios.remove(id-1);
-    }
-        
-    public static void añadirMunicipios(int id,String nombre){
-        municipios.add(new Municipio(id,nombre));
-    }
-    
     public static void main(String[] args) {
        llenarMunicipios();
        listaMunicipiosForE();
--- Tablas/src/main/java/models/Municipio.java
+++ Tablas/src/main/java/models/Municipio.java
@@ -1,6 +1,8 @@
 
 package models;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Gerardo Herrera
@@ -8,6 +10,8 @@ package models;
 public class Municipio {
     private int id;
     private String nombre;
+    static ArrayList<Municipio> municipios = new ArrayList<>();
+
 
     public Municipio() {
 
@@ -33,7 +37,41 @@ public class Municipio {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
+        public static void llenarMunicipios(){
+        municipios.add(new Municipio(1,"Calkini"));
+        municipios.add(new Municipio(2,"Campeche"));
+        municipios.add(new Municipio(3,"Carmen"));
 
+        System.out.println(municipios);
+        
+        
+    }
+    
+    
+    public static void listaMunicipiosForE(){
+        System.out.println("For each");
+        for (Municipio municipio : municipios) {
+            System.out.println(municipio);
+        }
+    }
+    
+    public static void listaMunicipiosForI(){
+        System.out.println("forI");
+        for (int i = 0; i < municipios.size(); i++) {
+            System.out.println(municipios.get(i).toString());
+        }
+    }
+    
+    public static void eliminarMunicipios(int id){
+        municipios.remove(id-1);
+    }
+        
+    public static void añadirMunicipios(int id,String nombre){
+        municipios.add(new Municipio(id,nombre));
+    }
+    
+    
     @Override
     public String toString() {
 
