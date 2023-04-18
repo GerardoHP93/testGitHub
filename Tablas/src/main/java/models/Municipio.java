56531bc02de4a6b35f6191ad0c1dcd139c0db41e
Gerardo Herrera <Gerardo Herrera@10.182.3.80>
2023-04-18 14:19:31 -0500

    Se crea objeto municipio

---
+++ Tablas/src/main/java/models/Municipio.java
@@ -0,0 +1,33 @@
+
+package models;
+
+/**
+ *
+ * @author Gerardo Herrera
+ */
+public class Municipio {
+    private int id;
+    private String nombre;
+
+    public Municipio() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
