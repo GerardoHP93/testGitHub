4d468354cd71a38b87ae5c9b6604202276592a20
Gerardo Herrera <Gerardo Herrera@10.182.3.80>
2023-04-18 15:18:22 -0500

    Métodos creae y eliminar objetos del arrayList

--- Tablas/src/main/java/com/mycompany/tablas/Main.java
+++ Tablas/src/main/java/com/mycompany/tablas/Main.java
@@ -26,8 +26,36 @@ public class Main {
     }
     
     
+    public static void listaMunicipiosForE(){
+        System.out.println("For each");
+        for (Municipio municipio : municipios) {
+            System.out.println(municipio);
+        }
+    }
+    
+    public static void listaMunicipiosForI(){
+        System.out.println("forI");
+        for (int i = 0; i < municipios.size(); i++) {
+            System.out.println(municipios.get(i).toString());
+        }
+    }
+    
+    public static void eliminarMunicipios(int id){
+        municipios.remove(id-1);
+    }
+        
+    public static void añadirMunicipios(int id,String nombre){
+        municipios.add(new Municipio(id,nombre));
+    }
+    
     public static void main(String[] args) {
        llenarMunicipios();
-       
+       listaMunicipiosForE();
+       System.out.println("");
+       listaMunicipiosForI();
+
+       eliminarMunicipios(3);
+       listaMunicipiosForI();
+      
     }
 }
