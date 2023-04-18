b2162c9b9ad8c6efefb7943cf14d276673f088ab
Gerardo Herrera <Gerardo Herrera@10.182.3.80>
2023-04-18 15:46:03 -0500

    Se cambio el nombre del proyecto

--- Tablas/src/main/java/com/mycompany/tablas/Main.java
+++ Tablas/src/main/java/com/mycompany/tablas/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tablas;
 
 import java.util.ArrayList;
@@ -13,16 +9,15 @@ import static models.Municipio.*;
  * @author Gerardo Herrera
  */
 public class Main {
-    
 
     public static void main(String[] args) {
-       llenarMunicipios();
-       listaMunicipiosForE();
-       System.out.println("");
-       listaMunicipiosForI();
+        llenarMunicipios();
+        listaMunicipiosForE();
+        System.out.println("");
+        listaMunicipiosForI();
 
-       eliminarMunicipios(3);
-       listaMunicipiosForI();
-      
+        eliminarMunicipios(3);
+        listaMunicipiosForI();
+
     }
 }
